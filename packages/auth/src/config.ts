c5811fc25c8a6566ab682de05ca04647d679ed8c
rdx <reydux95@gmail.com>
2025-10-01 04:31:06 +0200

    feat(auth): enable email & password sign up

--- packages/auth/src/config.ts
+++ packages/auth/src/config.ts
@@ -1,8 +1,14 @@
+import { config } from "@repo/config"
 import { dbConfig } from "@repo/db"
 import { betterAuth } from "better-auth"
 
 export const auth = betterAuth({
   database: dbConfig.pool,
+  emailAndPassword: {
+    enabled: true,
+    minPasswordLength: config.auth.password.minLength,
+    maxPasswordLength: config.auth.password.maxLength,
+  },
 
   // Use plural names for the tables
   user: { modelName: "users" },
